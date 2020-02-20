4a3f2bca2e2b8671cf3dc8088d47ab45a3c1b374
David Hacker <dmhacker@protonmail.com>
2020-02-19 17:12:09 -0800

    Fix problem 650 so that solutions work on clang now

--- include/650_two_keys_keyboard.hpp
+++ include/650_two_keys_keyboard.hpp
@@ -1,8 +1,53 @@
 #include <metal.hpp>
 
+#include <constants.hpp>
+
 namespace metaleet {
 
+// inner :: number -> number -> number -> pair<number>
+
+template <class ans, class n, class d, 
+    class guard = metal::same<metal::mod<n, d>, ZERO>>
+struct _inner_impl {
+    using type = typename _inner_impl<metal::add<ans, d>, 
+        metal::div<n, d>, d>::type;
+};
+
+template <class ans, class n, class d>
+struct _inner_impl<ans, n, d, metal::false_> {
+    using type = metal::pair<ans, n>;
+};
+
+template <class ans, class n, class d>
+using inner = typename _inner_impl<ans, n, d>::type;
+
+// outer :: number -> number -> number -> list<number> (3)
+
+template <class ans, class n, class d,
+         class guard = metal::greater<n, ONE>>
+struct _outer_impl {
+    using type = typename _outer_impl<metal::first<inner<ans, n, d>>, 
+        metal::second<inner<ans, n, d>>, metal::inc<d>>::type;
+};
+
+template <class ans, class n, class d>
+struct _outer_impl<ans, n, d, metal::false_> {
+    using type = metal::list<ans, n, d>; 
+};
+
+template <class ans, class n, class d>
+using outer = typename _outer_impl<ans, n, d>::type;
+
 // solve650 :: number -> number
+
+template <class n>
+struct _solve650_impl {
+    using type = metal::at<outer<ZERO, n, TWO>, ZERO>;
+};
+
+/*
+// My original solution does not work on clang-9!
+// However, it does compile correctly on g++ for some reason.
 
 template <class n>
 struct _solve650_impl {
@@ -28,9 +73,10 @@ struct _solve650_impl {
 };
 
 template <>
-struct _solve650_impl<metal::number<1>> {
-    using type = metal::number<0>;
+struct _solve650_impl<ONE> {
+    using type = ZERO;
 };
+*/
 
 template <class n>
 using solve650 = typename _solve650_impl<n>::type;
