8ffee9ad501399e40851d96ce04dd29c38c570df
David Hacker <dmhacker@protonmail.com>
2020-02-21 17:17:14 -0800

    Add more test cases to problem 662

--- src/662_tests.cpp
+++ src/662_tests.cpp
@@ -12,7 +12,15 @@ TEST_CASE("Test cases for problem #662")
                 btnode<metal::number<4>, btnode_end, btnode_end>>>()
         == 2);
     REQUIRE(solve662<btnode<metal::number<1>,
+                btnode<metal::number<2>, btnode<metal::number<3>, btnode_end, btnode_end>, btnode_end>,
+                btnode<metal::number<4>, btnode<metal::number<5>, btnode_end, btnode_end>, btnode_end>>>()
+        == 3);
+    REQUIRE(solve662<btnode<metal::number<1>,
                 btnode<metal::number<2>, btnode<metal::number<3>, btnode_end, btnode_end>, btnode<metal::number<4>, btnode_end, btnode_end>>,
                 btnode<metal::number<5>, btnode_end, btnode<metal::number<6>, btnode_end, btnode_end>>>>()
         == 4);
+    REQUIRE(solve662<btnode<metal::number<1>,
+                btnode<metal::number<2>, btnode<metal::number<3>, btnode<metal::number<4>, btnode_end, btnode_end>, btnode_end>, btnode_end>,
+                btnode<metal::number<5>, btnode_end, btnode<metal::number<6>, btnode_end, btnode<metal::number<7>, btnode_end, btnode_end>>>>>()
+        == 8);
 }
